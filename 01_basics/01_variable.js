2e0b7f8781b189963f4ce6c422461877449fb5d6
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-07-21 15:51:55 +0000

    new changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 1122 // this has a constant value
+let accountEmail = "xyz@google.com"
+var accountPassword = "123456"
+accountcity ="Bangalore"
+accountPassword="23455" 
+accountEmail="yze@google.com"
+let accountState;
+
+/*
+prefer to not use use var dut to block scope and functional scope
+
+*/
+
+// console.table
+console.table([accountEmail,accountId,accountPassword,accountState]);
+  
+"use strict"; // this treats all javascript code as new OR latest version
+
+// alert(3+3) // this statement runs only engine on but not on node.js
+
---
+++ 01_basics/02_dataconversion.js
@@ -0,0 +1,18 @@
+let score = null
+//console.log(typeof(score))
+
+let value = Number(score) // 
+console.log(typeof(value));
+
+
+console.log(typeof(value));
+console.log(value); // it says NaN -not a number  
+
+// type conversion 
+/* "33" => 
+
+
+
+
+
+    */
