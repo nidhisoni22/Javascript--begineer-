e46f3b062285423d6a8d5d662bcedfeb7e7cc218
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-07-21 16:00:34 +0000

    new changes

--- 01_basics/02_dataconversion.js
+++ 01_basics/02_dataconversion.js
@@ -9,10 +9,8 @@ console.log(typeof(value));
 console.log(value); // it says NaN -not a number  
 
 // type conversion 
-/* "33" => 
-
-
-
-
-
+/* "33" => converted to number
+"33abc" => not converted [NaN]
+true =. 1; 0 => false
+"hitesh " => true [any sting to true converted]
     */
