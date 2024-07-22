88aa3a96dad517ed926fdb24d4671040c10a9958
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-07-22 14:56:19 +0000

    DATA CONVERSION AND  OPERATORS

--- 01_basics/02_dataconversion.js
+++ 01_basics/02_dataconversion.js
@@ -1,12 +1,12 @@
 let score = null
 //console.log(typeof(score))
 
-let value = Number(score) // 
-console.log(typeof(value));
+let revalue = Number(score) // 
+//console.log(typeof(revalue));
 
 
-console.log(typeof(value));
-console.log(value); // it says NaN -not a number  
+//console.log(typeof(revalue));
+//console.log(revalue); // it says NaN -not a number  
 
 // type conversion 
 /* "33" => converted to number
@@ -14,3 +14,29 @@ console.log(value); // it says NaN -not a number
 true =. 1; 0 => false
 "hitesh " => true [any sting to true converted]
     */
+// ********************************OPERATIONS*********************************************************************************
+
+let value = 3 
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)  // addition
+// console.log(2-2)  // subtraction
+// // power
+// divide => / 
+// modulo %
+//  console.log(1 +"2");
+//  console.log(" 1" + 2);
+//  console.log(1+ 2 + "2")
+
+let x = 3
+const y = x++
+
+console.log(x);
+console.log(y)
+
+//  let a = 5
+//  const b = ++a
+ 
+//  console.log(a)
+//  console.log(b)
