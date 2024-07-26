847249a88307c6ae156a22b84914297b75097ecb
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-07-26 15:28:49 +0000

    Datatypes Summary

---
+++ 01_basics/dataypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive and Non-primitive
+// Javascript is dynamically typed language 
+//1.primitive - the primitive dataypes are call by value i,e.a copy is created for changes
+/* 7categories
+-String
+-Boolean
+-Number
+-Undefined
+-Null
+-Symbol
+-BigInt
+
+2.(NON-PRIMITIVE)Reference-
+-Arrays - written in square brackets
+-Objectes
+-Functions
+examples of SYMBOL
+
+
+*/
+// const Id=Symbol("123");
+// const Id1=Symbol("123")
+// console.log(Id === Id1); // returns false
+
+const heros=["ironman","shaktiman"]; // array
+ const score = undefined;
+let obj ={        //object
+    name:"nidhi",
+    age : "22",
+
+ }
+ const myfunc=function demo() {
+    console.log("hello world");
+    
+ }
+
+ // typeof Function
+   
+ console.log(typeof score);
+ 
+
