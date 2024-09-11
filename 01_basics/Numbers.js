6104c272b94969843dd125dd6f8a6b3224c7b40b
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-09-11 12:16:01 +0000

    Numbers

---
+++ 01_basics/Numbers.js
@@ -0,0 +1,23 @@
+
+// Number
+// const score=400;
+//  console.log((score));  // you cant get methods in this because score is a number but isnt defined as the datatype 
+// const balance =new Number(100);  // you can create a object of Number as well 
+// console.log(balance);
+ 
+// console.log(balance.toString()); // converts a number to string
+
+// console.log(balance.toFixed(2)); // gives a precision value for a given value
+// const num=112.787;
+// console.log(num.toPrecision(4)); // this gives precision on the numbers combined to both before (.) and after (.)
+// there are many methods related to Number
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Math
+console.log(Math);
+// //abs
+// console.log(Math.abs(-4)); 
+console.log(Math.round(4.6)); //round
+console.log(Math.ceil(4.2));  // ceil-chooses top value
+console.log(Math.floor(4.4));
