52fb45bc42a85b528c5a2a5876565d72b9ee53c9
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-09-19 17:35:58 +0000

    Objects basics

---
+++ 02_basics/objects.js
@@ -0,0 +1,35 @@
+// obejct literal
+
+//symbol
+const mysymbol=Symbol("123")
+
+
+const user ={
+    "Userno":"1",
+    [mysymbol]:"123",
+    myname:"Nidhi",
+    age:22,
+    location:"Bangalore"
+}
+// } //define object
+
+// //accessing object
+// console.log(user.myname);
+// console.log(user["Userno"]); // how you would access the " " 
+// console.log(user[mysymbol]);
+
+// Object.freeze(user);   //freeze()-->dosent allow any changes to be made on your object
+// user.myname="NIDHI";
+// console.log(user);
+
+//adding function with objects;
+user.greeting=function(){
+    console.log(`hello user`)
+}
+
+user.greeting1=function(){
+    console.log(`hello ${this.myname}`)
+}
+
+console.log(user.greeting()); // try it without the braces 
+console.log(user.greeting1());
