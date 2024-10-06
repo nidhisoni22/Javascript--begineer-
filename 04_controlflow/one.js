18a5326805390c536ec2ccf8852840626ede750f
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-10-06 12:24:40 +0000

    Switch case

---
+++ 04_controlflow/one.js
@@ -0,0 +1,62 @@
+//if
+
+// const temp=62
+// if(temp < 50){  //based on the condn here - true 
+//  //      
+//  console.log("executed")                                // -false
+
+// }
+// console.log("now tempertaure is greater");
+
+/******************************************************************************************************************************/       
+//else
+// if(temp < 50){  
+//     //      if true wil, be executed
+//     console.log("temperatue is less")                             
+   
+//    }
+//    else{
+//     console.log("now tempertaure is greater"); // if false will be exceuted
+//    }
+ 
+//    console.log("will be executed")
+
+/**************************************************************************************************************************** */
+//var use and block scope
+// const score=100
+// if(score<200){
+//     var power="fly"   // difference between the let and var
+//     console.log(`the bird ${power} in the sky`)
+// }
+// console.log(`the bird ${power} in the sky`)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// if-else-if
+// const balance =1000
+// if(balance < 500){
+//     console.log("is it less than");
+
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+    
+
+// }
+// else{
+//     console.log("less than 1200")
+// }
+ /****************************************************************************************************************/
+ // &&  -and /    || -or
+//&& to check one condition
+ price = true
+ member =false
+ if( price && member){
+    console.log("Allowed to purchase")
+ }
+
+ // || -->to check multiple condition
+ if( price || member){
+    console.log("Allowed to purchase")
+ }
---
+++ 04_controlflow/switch.js
@@ -0,0 +1,44 @@
+//switch case  -->basic syntax 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month =3
+// switch (month) {
+//     case 1:
+//         console.log("january");   
+//         break;
+//     case 2:
+//         console.log("febuaray");   
+//         break;
+//     case 3:
+//         console.log("march");   
+//         break;
+
+//     default:
+//         console.log("default")
+//         break;
+// }      
+// you can also add the string as your kay value
+const month ="march"
+switch (month) {
+    case "jan":
+        console.log("january");   
+        break;
+    case "feb":
+        console.log("febuaray");   
+        break;
+    case "march":
+        console.log("march");   
+        break;
+
+    default:
+        console.log("default")
+        break;
+}      
