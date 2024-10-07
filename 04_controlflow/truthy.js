e7fd5ca1ec343c94ae4dd9ee51779fdfd225e16e
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-10-07 16:37:32 +0000

    Control Flow

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -1,11 +1,11 @@
-const userEmail =[] 
-if(userEmail){ 
-    console.log("truthy values")
-}
-else{
-    console.log("not truee");
+// const userEmail =[] 
+// if(userEmail){ 
+//     console.log("truthy values")
+// }
+// else{
+//     console.log("not truee");
     
-}
+// }
 //falsy values -->Few values that are considered false
 // 0,-0, BigInt-0n ,"" ,null,undefined,NaN
 
@@ -20,8 +20,28 @@ else{
 // }
 
 // how to check for objects
-const myobj={}
-if(Object.keys(myobj).length === 0){
-console.log("empty obbj");
+// const myobj={}
+// if(Object.keys(myobj).length === 0){
+// console.log("empty obbj");
 
-}
+// }
+
+// Nullish coalesing operator(??) : null undefined
+let val1;
+//val1= 5 ?? 10
+// val1 =null ?? 10
+// val1=undefined ?? 10
+// val1=null ?? undefined
+// val2=undefined ?? null
+// clue1=null ?? 14
+// console.log(val1);
+// console.log(val2);
+// console.log(clue1);
+
+
+//ternary operator is different from the nullish operator
+//ternary operator is --> ? 
+//nullish operator is --> ?? 
+let price=90
+price >= 70 ? console.log("it is less than  ") : console.log("nah it is more");
+
