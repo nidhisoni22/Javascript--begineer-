30af2aa1db1beff77fbb1348fe4ed7954bf974e2
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-10-08 16:57:38 +0000

    For loop

--- 05_Iterations/loops.js
+++ 05_Iterations/loops.js
@@ -9,12 +9,31 @@
     
 // } // for with if
 
-// for 
-for (let i = 0; i < 10; i++) {
-    console.log(`print the value of i ${i}`);
+// for loop within loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(`print the value of i ${i}`);
     
-  for(let j=0;j<10;j++){
-    console.log(i + "*"+ j +'='+ i*j )
+//   for(let j=0;j<10;j++){
+//     console.log(i + "*"+ j +'='+ i*j )
+//   }
+    
+// }
+//for with arrays
+// let myarray=["flash","superman","ironman","thor"]
+// for(let i=0; i <myarray.length ; i++){
+//   const element=myarray[i];
+//   //console.log(element);
+  
+//}
+// special keyword
+// break and continue
+for (let i = 1; i <= 20 ; i++) {
+  const element = i;
+  if(i == 5){
+  console.log("5 detected")
+    continue;
   }
-    
+  
+  console.log(element);
+  
 }
