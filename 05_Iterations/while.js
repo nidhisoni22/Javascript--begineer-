fe4320a0d8d78b2cd980618c6d3580ac2dc5f6b0
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-10-08 17:10:10 +0000

    while loop

---
+++ 05_Iterations/while.js
@@ -0,0 +1,23 @@
+// while 
+let i=0
+while (i<50) {
+  
+    console.log(i)
+    i=i+5;
+}
+
+const myarr=['flash','batman','thor']
+let arr=0
+while(arr<myarr.length){
+    console.log(`value of array is ${myarr[arr]}`)
+    arr++;
+}
+
+/********************************************************************************************************************************/
+//do while-->executes the loop atleast once
+score=11
+do {
+    console.log(`score value is ${score}`);
+    score++;
+
+}while(score<=10)
