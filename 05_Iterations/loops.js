fa3b0c5eafa1a496943f8429ce6d695a3c23f27f
nidhisoni22 <143543679+nidhisoni22@users.noreply.github.com>
2024-10-08 16:25:58 +0000

    Loops

---
+++ 05_Iterations/loops.js
@@ -0,0 +1,20 @@
+// for 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 just got printed");
+//     }
+//     console.log(element);
+    
+// } // for with if
+
+// for 
+for (let i = 0; i < 10; i++) {
+    console.log(`print the value of i ${i}`);
+    
+  for(let j=0;j<10;j++){
+    console.log(i + "*"+ j +'='+ i*j )
+  }
+    
+}
